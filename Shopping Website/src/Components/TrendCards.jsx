f305edfb28ac56d2d849eba335917cd958b101ed
Shemaremy <remyshema20@gmail.com>
2024-06-08 20:56:38 +0300

    commit 007

--- Shopping Website/src/Components/TrendCards.jsx
+++ Shopping Website/src/Components/TrendCards.jsx
@@ -1,28 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import './Main.css';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-
 
 
 
 const TrendCards = () => {
-
-
-    const [index, setIndex] = useState(0);
-
-    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    const nextCard = () => {
-        if (index < cards.length - 1) {
-            setIndex(index + 1);
-        }
-    };
-
-    const prevCard = () => {
-        if (index > 0) {
-            setIndex(index - 1);
-        }
-    };
 
 
 
@@ -30,21 +11,15 @@ const TrendCards = () => {
         <>
             <div className="Upper_trending_part"></div>
             <div className="Lower_trending_part">
-                <p className="Slide_left_customers" onClick={prevCard}>
+                <p className="Slide_left_customers">
                     <i className="sld_L_C fas fa-chevron-left"></i>
                 </p>
                 <div className="Trending_items_library">
-                    <TransitionGroup component={null}>
-                        <CSSTransition
-                            key={cards[index]}
-                            timeout={500}
-                            classNames="card"
-                        >
-                            <div className="trend_card">{cards[index]}</div>
-                        </CSSTransition>
-                    </TransitionGroup>
+                    <div className="trend_card">1</div>
+                    <div className="trend_card">2</div>
+                    <div className="trend_card">3</div>
                 </div>
-                <p className="Slide_right_customers" onClick={nextCard}>
+                <p className="Slide_right_customers">
                     <i className="sld_L_C fas fa-chevron-right"></i>
                 </p>
             </div>
