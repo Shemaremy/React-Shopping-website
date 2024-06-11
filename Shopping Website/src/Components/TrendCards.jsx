0485037857d582499b4cdeed0400ba67fd045d43
Shemaremy <remyshema20@gmail.com>
2024-06-11 16:56:29 +0300

    commit 007

--- Shopping Website/src/Components/TrendCards.jsx
+++ Shopping Website/src/Components/TrendCards.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import './Main.css';
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const TrendCards = () => {
+
+    const settings = {
+        dots: false,
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "0px",
+        slidesToShow: 3,
+        speed: 500
+      };
 
 
 
@@ -11,17 +23,28 @@ const TrendCards = () => {
         <>
             <div className="Upper_trending_part"></div>
             <div className="Lower_trending_part">
-                <p className="Slide_left_customers">
-                    <i className="sld_L_C fas fa-chevron-left"></i>
-                </p>
-                <div className="Trending_items_library">
-                    <div className="trend_card">1</div>
-                    <div className="trend_card">2</div>
-                    <div className="trend_card">3</div>
+                <div className="slider-container-2">
+                    <Slider {...settings}>
+                    <div className="Trend-2">
+                        <div className="card-2">1</div>
+                    </div>
+                    <div className="Trend-2">
+                        <div className="card-2">2</div>
+                    </div>
+                    <div className="Trend-2">
+                        <div className="card-2">3</div>
+                    </div>
+                    <div className="Trend-2">
+                        <div className="card-2">4</div>
+                    </div>
+                    <div className="Trend-2">
+                        <div className="card-2">5</div>
+                    </div>
+                    <div className="Trend-2">
+                        <div className="card-2">6</div>
+                    </div>
+                    </Slider>
                 </div>
-                <p className="Slide_right_customers">
-                    <i className="sld_L_C fas fa-chevron-right"></i>
-                </p>
             </div>
         </>
     );
