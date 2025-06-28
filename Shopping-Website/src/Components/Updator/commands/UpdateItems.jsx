01ff29fbc68c1cf5bb14cc9b08059090813a0971
Shemaremy <remyshema20@gmail.com>
2025-06-28 15:00:42 +0200

    Did the update part

--- Shopping-Website/src/Components/Updator/commands/UpdateItems.jsx
+++ Shopping-Website/src/Components/Updator/commands/UpdateItems.jsx
@@ -7,12 +7,14 @@ const UpdateItems = () => {
 
   const categories = [
     { key: 'all', label: 'View All', query: '' },
-    { key: 'german_shepherd', label: 'German Shepherds', query: 'German Shepherd' },
-    { key: 'bulldog', label: 'Bulldogs', query: 'Bulldog' },
-    { key: 'husky', label: 'Siberian Huskies', query: 'Siberian Husky' },
-    { key: 'malamute', label: 'Alaskan Malamutes', query: 'Alaskan Malamute' },
-    { key: 'poodle', label: 'Poodles', query: 'Poodle' }
+    { key: 'shoes', label: 'Shoes', query: 'Shoes' },
+    { key: 'hoodies', label: 'Hoodies', query: 'Hoodies' },
+    { key: 'jackets', label: 'Jackets', query: 'Jackets' },
+    { key: 'tshirts', label: 'T-shirts', query: 'T-shirts' },
+    { key: 'pants', label: 'Pants', query: 'Pants' },
+    { key: 'caps', label: 'Caps', query: 'Caps' }
   ];
+
 
   const [button, setButton] = useState();
   const [totalItems, setTotalItems] = useState(0);
@@ -26,7 +28,7 @@ const UpdateItems = () => {
   const [passedId, setPassedId] = useState('');
 
   const [showIframe, setShowIframe] = useState(true);
-  const [iframeSrc, setIframeSrc] = useState("https://remydp.netlify.app/kpuppies");
+  const [iframeSrc, setIframeSrc] = useState("https://remydp.netlify.app/verve");
 
 
   const isActive = (panel) => button === panel ? 'active' : '';
@@ -37,40 +39,20 @@ const UpdateItems = () => {
 
   // Input fields container
   const fields = [
-    { label: 'Name', name: 'name', type: 'text', placeholder: 'Ex: Fiston' },
-    { label: 'Price', name: 'price', type: 'number', placeholder: 'Ex: 400000' },
+    { label: 'Name', name: 'name', type: 'text', placeholder: 'Ex: Jordan 1 Black' },
+    { label: 'Price', name: 'price', type: 'number', placeholder: 'Ex: 35000' },
     { label: 'Quantity', name: 'quantity', type: 'number', placeholder: 'Ex: 10' },
-    { label: 'Description', name: 'description', type: 'text', placeholder: 'Ex: Nice quality' },
+    { label: 'Size', name: 'size', type: 'text', placeholder: 'Ex: 40, 41, S, M, XL,...' },
   ];
 
 
   // Dropdown fields container
   const dropdowns = [
     {
-      label: 'Type',
-      name: 'type',
-      options: ['German Shepherd', 'Bulldog', 'Siberian Husky', 'Alaskan Malamute', 'Poodle'],
-    },
-    {
-      label: 'Age',
-      name: 'age',
-      options: ['Less than 5 months', '5 - 12 months', '1 - 5 years'],
-    },
-    {
-      label: 'Gender',
-      name: 'gender',
-      options: ['Male', 'Female'],
-    },
-    {
-      label: 'Plug',
-      name: 'plug',
-      options: ['IGOBE', 'ANTOINE'],
-    },
-    {
-      label: 'Reservatiom',
-      name: 'reservation',
-      options: ['True', 'False'],
-    },
+      label: 'Category',
+      name: 'category',
+      options: ['Shoes', 'Pants', 'T-shirts', 'Hoodies', 'Jackets', 'Caps'],
+    }
   ];
 
 
@@ -79,7 +61,6 @@ const UpdateItems = () => {
     ...Object.fromEntries(fields.map(f => [f.name, ''])),
     ...Object.fromEntries(dropdowns.map(d => [d.name, ''])),
     image: '',
-    subImages: ['', '', ''],
   };
 
   const [newItem, setNewItem] = useState(initialFormState);
@@ -106,8 +87,8 @@ const UpdateItems = () => {
 
   // -- Function to fetch data --------------------------------------------------------------
 
-  // const URL = 'https://kigali-puppies.glitch.me';
-  const URL = 'https://kigalipuppies.up.railway.app';
+  const URL = 'https://verve-users.glitch.me';
+  
 
   const fetchData = async (categoryKey, categoryQuery) => {
     const disableButton = document.querySelector('.nav-button');
@@ -117,7 +98,7 @@ const UpdateItems = () => {
     setButton(categoryKey);
 
     const endpoint = categoryQuery 
-    ? `${URL}/api/admindisplay?type=${encodeURIComponent(categoryQuery)}`
+    ? `${URL}/api/admindisplay?category=${encodeURIComponent(categoryQuery)}`
     : `${URL}/api/admindisplay`;
 
     try {
@@ -211,13 +192,6 @@ const UpdateItems = () => {
         };
     }, []);
 
-
-      // Handle change of sub images
-      const handleSubImageChange = (index, value) => {
-        const updated = [...newItem.subImages];
-        updated[index] = value;
-        setNewItem({ ...newItem, subImages: updated });
-      };
       
       
       // Handle change for normal products values
@@ -227,6 +201,29 @@ const UpdateItems = () => {
       };
 
 
+      const handleSizeInput = (sizeInput) => {
+        const validSize = sizeInput.replace(/[^a-zA-Z0-9,\s]/g, '').toUpperCase();
+      
+        let sizeArray = validSize.split(/[,\s]+/).filter(Boolean);
+        const validSizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+      
+        sizeArray = sizeArray.filter(size => validSizes.includes(size) || /^\d+$/.test(size));
+      
+        if (sizeArray.length === 0) {
+          alert("Please enter valid size values: XS, S, M, L, XL, XXL, or numeric values (46, 27).");
+          return '';
+        }
+      
+        const isNumeric = sizeArray.every(size => !isNaN(size));
+      
+        if (isNumeric) {
+          sizeArray = [...new Set(sizeArray)].sort((a, b) => parseFloat(a) - parseFloat(b));
+        } else {
+          sizeArray = [...new Set(sizeArray)].sort((a, b) => validSizes.indexOf(a) - validSizes.indexOf(b));
+        }
+      
+        return sizeArray.join(', ');
+    };
 
     const handleUpdateItem = async () => {
         const bringPage = document.querySelector('.update-page');
@@ -236,30 +233,25 @@ const UpdateItems = () => {
         setUpdateLoad(true);
 
         const updatedFields = {};
-        const keys = ['name', 'type', 'age', 'gender', 'reserved', 'price', 'quantity', 'description', 'image', 'plug'];
+        const keys = [ 'name', 'category', 'price', 'quantity', 'image' ];
 
         keys.forEach(key => {
           if (newItem[key]) updatedFields[key] = newItem[key];
         });
 
 
-        // if (subImages) updatedFields.subImages = subImages;
-        // if (subImages && Array.isArray(subImages) && subImages.some(img => img)) {
-        //   updatedFields.subImages = subImages;
-        // }
-
-
-        // Handling proper allocating of sub image urls
-        const currentProduct = products.find(p => p._id === passedId);
-        const cleanedSubImages = newItem.subImages.map((img, idx) => {
-          return img === '' && currentProduct?.subImages?.[idx] ? currentProduct.subImages[idx] : img;
-        });
-
-
-        // Only set subImages if at least one is valid
-        if (cleanedSubImages.some(img => img)) {
-          updatedFields.subImages = cleanedSubImages;
+        // Handle 'size' separately
+        if (newItem.size) {
+          const formattedSize = handleSizeInput(newItem.size);
+          if (!formattedSize) {
+            setUpdateLoad(false);
+            disableButton.classList.remove('disable');
+            setIsButtonDisabled(false);
+            return;
+          }
+          updatedFields.size = formattedSize;
         }
+
         
 
         
@@ -299,7 +291,7 @@ const UpdateItems = () => {
             setNewItem(initialFormState);
 
             setIframeSrc("");
-            setTimeout(() => setIframeSrc("https://remydp.netlify.app/kpuppies"), 50);
+            setTimeout(() => setIframeSrc("https://remydp.netlify.app/verve"), 50);
 
             bringPage.classList.remove('is-active');
             
@@ -354,9 +346,6 @@ const UpdateItems = () => {
               </div>
             )}
             <input type="text" name="image" onChange={handleChange} value={newItem.image} placeholder='MAIN IMAGE' />
-            <input type="text" onChange={(e) => handleSubImageChange(0, e.target.value)} value={newItem.subImages[0]} placeholder='Sub Image 1' />
-            <input type="text" onChange={(e) => handleSubImageChange(1, e.target.value)} value={newItem.subImages[1]} placeholder='Sub Image 2' />
-            <input type="text" onChange={(e) => handleSubImageChange(2, e.target.value)} value={newItem.subImages[2]} placeholder='Sub Image 3' />
           </div>
 
 
@@ -439,20 +428,16 @@ const UpdateItems = () => {
       {[...products]
         //.sort((a, b) => a.name.localeCompare(b.name)) Names A - Z
         //.sort((a, b) => a.price - b.price) // Price A - Z
-        //.sort((a, b) => a.quantity - b.quantity) // Quantity A - Z
-        .sort((a, b) => a.plug.localeCompare(b.plug))
+        .sort((a, b) => a.quantity - b.quantity) // Quantity A - Z
         .map((item, index) => (
           <div key={index} className="product-card" onClick={() => handleBringPage(item.name, item._id)}>
             <img src={item.image} alt={item.name} className="product-image" />
             <h3 className="product-name">{item.name}</h3>
-            <p className="product-category">Type: {item.type}</p>
-            <p className="product-age">Age range: {item.age}</p>
-            <p className="product-size">Gender: {item.gender}</p>
+            <p className="product-category">Category: {item.category}</p>
+            <p className="product-size">Sizes: {item.size}</p>
             <p className="product-quantity">Quantity: {item.quantity}</p>
-            <p className="product-description">Description: {item.description}</p>
             <p className="product-price">Price: {Number(item.price).toLocaleString("en-US")}</p>
-            <p className="product-plug">Plug: {item.plug}</p>
-        </div>
+          </div>
       ))}
     </>
   );
